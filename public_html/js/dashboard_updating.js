0a3dd9e65041dfbc9001836237168dee5fb3a2e1
Jonatan Silva <jonatan.silva4@fatec.sp.gov.br>
2020-06-16 22:40:59 +0000

    Dashboard
    adicionado labels para representaçao dos hosts

--- public_html/js/dashboard_updating.js
+++ public_html/js/dashboard_updating.js
@@ -1,23 +1,16 @@
 setInterval( () => {
-    fetch(
-      '../shell/checagem.sh',
-      {
-        method: 'get',
-      }
-    )
-    .then( response => {
-      return response.json()
-    })
-    .then( result => {
-      if(result.pc1_status === 0) {
-        pc_1 = document.getElementsByClassName('pc')[0]
-        pc_1.style.backgroundColor = "#0dff00"
-      } else {
-        pc_1 = document.getElementsByClassName('pc')[0]
-        pc_1.style.backgroundColor = "#fc0303"
-      }
-    })
+    fetch('../shell/checagem.sh')
+      .then( response => {
+        return response.json()
+      })
+      .then( result => {
+        if(result.pc1_status === 0) {
+          pc_1 = document.getElementsByClassName('pc')[0]
+          pc_1.style.backgroundColor = "#0dff00"
+        } else {
+          pc_1 = document.getElementsByClassName('pc')[0]
+          pc_1.style.backgroundColor = "#fc0303"
+        }
+      })
   },
   1000)
-
-
